5367886cdc7f84acefbfcbc49a1de7477a49e847
Andrew Wint <wint.andrew@gmail.com>
2019-03-25 11:47:19 -0700

    Update animal_height_weights_controller.rb
    Clean up from experimenting

--- app/controllers/animal_height_weights_controller.rb
+++ app/controllers/animal_height_weights_controller.rb
@@ -3,15 +3,8 @@ class AnimalHeightWeightsController < ApplicationController
     @animals = AnimalHeightWeight.new
   end
 
-  def animal
-    @guess = Guess.new(guess_params)
-    @result = @guess.guess_animal(@guess.height, @guess.weight, @guess.bias)
-    render :layout => false
-  end
-
   private
   def guess_params
     params.permit(:height, :weight, :animal_height_weight)
   end
-
 end
